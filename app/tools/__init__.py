fa4b9fc76156828d23384a21ce706384f38d1113
Kabeer Khan <kdotkhan22@gmail.com>
2026-02-02 21:56:47 +0500

    .logs Added to gitignore

---
+++ app/tools/__init__.py
@@ -0,0 +1,12 @@
+
+from .web_search import (
+    search_web,
+    search_web_with_retry,
+    validate_search_query
+)
+
+__all__ = [
+    'search_web',
+    'search_web_with_retry',
+    'validate_search_query'
+]
